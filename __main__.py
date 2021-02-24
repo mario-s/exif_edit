6f7f3de2eeabbbd64c4e9f3a765a6feee9397d83
Mario <ma.schroed73@gmail.com>
2021-02-24 21:37:00 +0100

    fix default img

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from exif_edit.app import App
 
 if __name__ == "__main__":
     def img_path():
-        img_path = os.path.realpath('./exif_edit/test/resources/lookup.jpg')
+        img_path = os.path.realpath('./test/resources/lookup.jpg')
         args_len = len(sys.argv) - 1
         if args_len > 0:
             img_path = os.path.realpath(sys.argv[1])
