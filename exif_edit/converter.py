95918efde4c3fff56590ded4e025359a6acb754d
Mario <ma.schroed73@gmail.com>
2021-02-26 09:25:43 +0100

    more dictionaries

--- exif_edit/converter.py
+++ exif_edit/converter.py
@@ -4,26 +4,27 @@ from exif import ColorSpace, ResolutionUnit, Orientation
 class Converter:
     def __init__(self):
         #the first key, value pair is used as a fall back for an illegal input
-        self.color_spaces = {"2": ColorSpace.UNCALIBRATED, "1": ColorSpace.SRGB}
-        self.resolution_units = {"2": ResolutionUnit.INCHES, "3": ResolutionUnit.CENTIMETERS}
-        self.orientations = {
+        color_spaces = {"2": ColorSpace.UNCALIBRATED, "1": ColorSpace.SRGB}
+        resolution_units = {"2": ResolutionUnit.INCHES, "3": ResolutionUnit.CENTIMETERS}
+        orientations = {
             "1": Orientation.TOP_LEFT, "2": Orientation.TOP_RIGHT,
             "3": Orientation.BOTTOM_RIGHT, "4": Orientation.BOTTOM_LEFT,
             "5": Orientation.LEFT_TOP, "6": Orientation.RIGHT_TOP,
             "7": Orientation.RIGHT_BOTTOM, "8": Orientation.LEFT_BOTTOM}
 
+        self.dictionary = {"color_space": color_spaces, 
+            "resolution_unit": resolution_units,
+            "orientation": orientations}
+
     def to_enumeration(self, key, value):
-        if key == "color_space":
-            return self.__lookup(self.color_spaces, value)
-        elif key == "resolution_unit":
-            return self.__lookup(self.resolution_units, value)
-        elif key == "orientation":
-            return self.__lookup(self.orientations, value)
+        if key in self.dictionary:
+            dict = self.dictionary[key]
+            return self.__lookup(dict, value)
         return value
 
     def __lookup(self, dict, value):
-        try:
+        if value in dict:
             return dict[value]
-        except:
-            k = next(iter(dict))
-            return dict[k]
+        #return first
+        k = next(iter(dict))
+        return dict[k]
