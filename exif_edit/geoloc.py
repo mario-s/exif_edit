3df8764e9d50856276962dd86623e4db4bb0e42c
Mario <ma.schroed73@gmail.com>
2021-03-24 18:36:39 +0100

    add more possibilities to create format

--- exif_edit/geoloc.py
+++ exif_edit/geoloc.py
@@ -1,32 +1,33 @@
-class LocationFormat:
+"""
+This class holds the value of the latitude or longitude used in coordinates.
+"""
+class Degree:
     def __init__(self, value):
-        if isinstance(value, tuple):
+        if value is None:
+            raise ValueError("expected at value")
+
+        if self.__is_collection(value):
             #degrees, minutes, seconds
             if len(value) == 3:
-                self.format = self.__dms2decimal(value[0], value[1], value[2])
-            #degrees and decimal minutes
-            else:
-                pass
-        #decimal degrees
+                self.format = self.__dms2dd(value[0], value[1], value[2])
         else:
+            #decimal degrees
             self.format = value
 
     @classmethod
-    def __dms2decimal(cls, degrees, minutes, seconds):
+    def __is_collection(cls, value):
+        return isinstance(value, tuple) or isinstance(value, list)
+
+    @classmethod
+    def __dms2dd(cls, degrees, minutes, seconds):
         """ 
         Converts degrees, minutes, and seconds to decimal degrees.
         """
-
-        dec = degrees
         min = minutes/60
         sec = seconds/3600
         if degrees >= 0:
-            dec = dec + min + sec
-        else:
-            dec = dec - min - sec
-        
-        return round(dec, 6)
-            
+            return degrees + min + sec
+        return degrees - min - sec
 
     def decimalDegrees(self):
-        return self.format
+        return round(self.format, 6)
--- test/test_geoloc.py
+++ test/test_geoloc.py
@@ -1,11 +1,19 @@
 import unittest
 
-from exif_edit.geoloc import LocationFormat
+from exif_edit.geoloc import Degree
 
 class TestLocationFormat(unittest.TestCase):
 
-    def test_dms_to_dd(self):
-        loc = LocationFormat((78, 55, 44.33324))
+    def test_dms_to_dd_tuple(self):
+        loc = Degree((78, 55, 44.33324))
+        self.assertEqual(78.928981, loc.decimalDegrees())
+
+    def test_dms_to_dd_list(self):
+        loc = Degree([78, 55, 44.33324])
+        self.assertEqual(78.928981, loc.decimalDegrees())
+
+    def test_dd(self):
+        loc = Degree(78.92898123)
         self.assertEqual(78.928981, loc.decimalDegrees())
 
 
