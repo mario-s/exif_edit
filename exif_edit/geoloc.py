2590a0a00a1858f37386016e6351cca39ac889cf
Mario <ma.schroed73@gmail.com>
2021-03-25 20:11:51 +0100

    add class for decimal format

--- exif_edit/geoloc.py
+++ exif_edit/geoloc.py
@@ -1,7 +1,10 @@
 """
-This class holds the value of the latitude or longitude used in coordinates.
+This module contains classes related to coordinates.
 """
 class DmsFormat:
+    """
+    Degree given in DMS format.
+    """
     def __init__(self, value):
         if len(value) != 3:
             raise ValueError("expected (degree, minuntes, seconds)")
@@ -10,10 +13,7 @@ class DmsFormat:
         self.minutes = int(value[1])
         self.seconds = value[2]
 
-    def __dms2dd(self):
-        """ 
-        Converts degrees, minutes, and seconds to decimal degrees.
-        """
+    def __dms2dec(self):
         min = self.minutes/60
         sec = self.seconds/3600
         if self.degrees >= 0:
@@ -21,7 +21,33 @@ class DmsFormat:
         return self.degrees - min - sec
 
     def decimalDegrees(self):
-        return round(self.__dms2dd(), 6)
+        """ 
+        Converts degrees, minutes, and seconds to decimal degrees.
+        """
+        return round(self.__dms2dec(), 6)
 
     def __repr__(self) -> str:
         return f"{self.degrees}°{self.minutes}\'{self.seconds}\""
+
+class DecimalFormat:
+    """
+    Degree given in Decimal format.
+    """
+    def __init__(self, value):
+        if value is None:
+            raise ValueError("expected degree in decimal")
+
+        self.degrees = value
+
+    def dmsDegrees(self):
+        """ 
+        Converts decimal degrees to (degrees, minutes, and seconds).
+        """
+        deg = int(self.degrees)
+        min = int((self.degrees - deg) * 60)
+        sec = (self.degrees - deg - min/60) * 3600
+        
+        return deg, min, round(sec, 6)
+        
+    def __repr__(self) -> str:
+        return f"{round(self.degrees, 6)}°"
--- test/test_geoloc.py
+++ test/test_geoloc.py
@@ -1,11 +1,12 @@
 import unittest
 
-from exif_edit.geoloc import DmsFormat
+from exif_edit.geoloc import DmsFormat, DecimalFormat
 
 class TestDmsFormat(unittest.TestCase):
 
     def test_wrong_constructor_arg(self):
         self.assertRaises(ValueError, lambda: DmsFormat([1]))
+        self.assertRaises(ValueError, lambda: DecimalFormat(None))
 
     def test_dms_to_dd_tuple(self):
         loc = DmsFormat((78, 55, 44.33324))
@@ -15,9 +16,17 @@ class TestDmsFormat(unittest.TestCase):
         loc = DmsFormat([78, 55, 44.33324])
         self.assertEqual(78.928981, loc.decimalDegrees())
 
-    def test_to_string(self):
+    def test_dms_to_string(self):
         loc = DmsFormat([78.0, 55.0, 44.33324])
         self.assertEquals("78°55\'44.33324\"", loc.__repr__())
+
+    def test_dec_to_dms(self):
+        loc = DecimalFormat(30.263888889)
+        self.assertEquals((30, 15, 50), loc.dmsDegrees())
+
+    def test_dec_to_string(self):
+        loc = DecimalFormat(30.263888889)
+        self.assertEquals("30.263889°", loc.__repr__())
 
 
 if __name__ == '__main__':
